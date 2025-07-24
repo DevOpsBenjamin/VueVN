e929a9562d0ed34463a33e97a40901dda034607b
DevOpsBen <devops.benjamin@gmail.com>
2025-07-25 01:46:46 +0200

    FIX a tiny comment

--- src/engine/stores/gameState.js
+++ src/engine/stores/gameState.js
@@ -1,9 +1,12 @@
 import { defineStore } from 'pinia';
 
+// THIS IS A PLACEHOLDER FOR EACH PROJECT'S GAME STATE
+// It should be customized per project requirements using plugin capacity
+// It's only defined here to make the engine base code having a default store
+
 const useGameState = defineStore('gameState', {
   state: () => ({
     player: { name: '' },
-    flags: {},
   }),
 });
 export default useGameState;
