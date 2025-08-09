c622087b32aae6f84277b65c19be5615427a1497
DevOpsBen <devops.benjamin@gmail.com>
2025-08-09 19:55:13 +0200

    event update

--- projects/sample/events/start/intro.ts
+++ projects/sample/events/start/intro.ts
@@ -7,30 +7,34 @@ const intro: VNEvent = {
   unlocked: () => true,
   locked: (state) => state.flags.introSeen,
   async execute(engine, state) {
-    engine.setForeground("assets/images/background/intro/hall.png");
-    await engine.showText("Welcome to sample!");
-    await engine.showText("This is your first event.");
-    let choice = "";
-    while (choice !== "start") {
+    engine.setForeground('assets/images/background/intro/hall.png');
+    await engine.showText('Welcome to sample!');
+    await engine.showText('This is your first event.');
+    let choice = '';
+    // Loop until the user chooses to start the adventure
+    // This is a sample to handle go back on previous choices when the user make a choice not progressing the story
+    // In most choice events, you would not need this loop
+    // and would just handle the choice directly.
+    while (choice !== 'start') {
       choice = await engine.showChoices([
-        { text: "Start the adventure", id: "start" },
-        { text: "Learn more", id: "learn" },
+        { text: 'Start the adventure', id: 'start' },
+        { text: 'Learn more', id: 'learn' },
       ]);
 
-      if (choice === "learn") {
+      if (choice === 'learn') {
         await engine.showText(
-          "VueVN is a visual novel engine built with Vue 3.",
+          'VueVN is a visual novel engine built with Vue 3.'
         );
         await engine.showText(
-          "You can create your own stories by adding events and assets.",
+          'You can create your own stories by adding events and assets.'
         );
-        await engine.showChoices([{ text: "Return", id: "return" }]);
+        await engine.showChoices([{ text: 'Return', id: 'return' }]);
       }
     }
 
     await engine.showText("Great! Let's begin your adventure.");
     state.flags.introSeen = true;
-    state.location = "bedroom";
+    state.location = 'bedroom';
   },
 };
 
