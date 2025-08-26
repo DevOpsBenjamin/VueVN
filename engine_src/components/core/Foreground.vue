108e932c1dc8a9ae9ddf6e4ff6d030fba63e3354
vscode <vscode@codeserver.local>
2025-08-26 09:18:49 +0000

    fix

--- engine_src/components/core/Foreground.vue
+++ engine_src/components/core/Foreground.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    v-show="engineState.foreground"
+    v-show="engineState.foreground != null && engineState.foreground.length > 0"
     class="absolute inset-0 w-full h-full pointer-events-none z-30"
   >
     <!-- Foreground image -->
     <img
-      :src="engineState.foreground![0]"
+      :src="engineState.foreground?.[0]"
       class="object-contain w-full h-full"
       alt="Foreground"
     />
@@ -15,4 +15,5 @@
 <script setup lang="ts">
 import { engineState as useEngineState } from '@generate/stores';
 const engineState = useEngineState();
+console.log(engineState.$state);
 </script>
