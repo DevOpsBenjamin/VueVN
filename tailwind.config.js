32e67958663b5f56917072669e91ad33a4ce1921
DevOpsBen <devops.benjamin@gmail.com>
2025-08-26 03:32:56 +0200

    Fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-    './public/game/events/**/*.js'
+    './engine.html',
+    './editor.html',
+    './engine_src/**/*.{vue,js,ts,jsx,tsx}',
+    './editor_src/**/*.{vue,js,ts,jsx,tsx}',
+    './public/game/events/**/*.js',
   ],
   theme: {
     extend: {
       zIndex: {
-        '60': '60',
-        '70': '70',
-        '80': '80',
-        '90': '90',
-      }
+        60: '60',
+        70: '70',
+        80: '80',
+        90: '90',
+      },
     },
   },
   plugins: [],
-}
+};
